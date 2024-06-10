012f637080995f57f911f05e8ba8ea4bd6b55ec9
Arulkumaran <arulkumaran0108@gmail.com>
2024-06-10 18:24:30 +0800

    updated the projects section

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -22,24 +22,27 @@ const Projects = () => {
     return () => clearInterval(intervalId);
   }, []); // Run effect only once on mount
   return (
-    <div
-      className=" flex flex-col items-center justify-center "
-      id="projects"
-    >
+    <div className=" flex flex-col items-center justify-center " id="projects">
       <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
         My Projects
       </h1>
       <div className="h-full md:w-full flex flex-1 mb-5 flex-col md:flex-row gap-10 px-10">
-        <ProjectCard
+        {/* <ProjectCard
           src={imageUrls[currentImageIndex]}
           title="Smart Agri Dashboard"
           description="A responsive and intuitive platform tailored specifically for agricultural monitoring "
           repoLink="https://github.com/AK1198?tab=repositories"
           demoLink="https://github.com/AK1198?tab=repositories"
-        />
-
+        /> */}
         <ProjectCard
-          src="/weather_app.PNG"
+          src="/house_image.jpg"
+          title="House Price Prediction"
+          description=" A website offering price prediction for houses in Boston . "
+          repoLink="https://github.com/AK1198/bostonhousepricing"
+          demoLink="http://3.104.63.239/"
+        />
+        <ProjectCard
+          src="/weather_image.jpg"
           title="Weather Forecast"
           description=" A website offering comprehensive real-time weather insights ,utilizing data sourced from a reliable public API. "
           repoLink="https://github.com/AK1198/weather-app-nextjs"
